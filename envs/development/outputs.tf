f4b85b14771dbda8c3fe4e248079cfa37e73fc04
n.fujita <nobuyuf@amazon.co.jp>
2021-09-19 04:00:38 +0900

    add route table outputs

--- envs/development/outputs.tf
+++ envs/development/outputs.tf
@@ -16,3 +16,8 @@ output "vpc-3az-publicsub2" {
 output "vpc-3az-publicsub3" {
   value = module.vpc-3az.publicsub3
 }
+
+
+output "vpc-3az-private3-rtb" {
+  value = module.vpc-3az.private3-rtb
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,9 +1,9 @@
-#--vpc
+#--a vpc
 output "vpc" {
   value = aws_vpc.this
 }
 
-#--subnet
+#--subnets
 output "publicsub1" {
   value = aws_subnet.publicsub1
 }
@@ -28,3 +28,19 @@ output "privatesub3" {
   value = element(concat(aws_subnet.privatesub3.*, tolist([""])), 0)
 }
 
+#--route tables
+output "public-rtb" {
+  value = aws_route_table.public
+}
+
+output "private1-rtb" {
+  value = aws_route_table.private1
+}
+
+output "private2-rtb" {
+  value = aws_route_table.private2
+}
+
+output "private3-rtb" {
+  value = element(concat(aws_route_table.private3.*, tolist([""])), 0)
+}
