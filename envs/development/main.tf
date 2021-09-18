0f1230f9c9fe953dbde930f7d2b2dceaad46cba8
n.fujita <nobuyuf@amazon.co.jp>
2021-09-19 03:54:17 +0900

    add routes

--- envs/development/main.tf
+++ envs/development/main.tf
@@ -23,3 +23,27 @@ module "vpc-2az" {
 
   vpcflowlogsbucket = "arn:aws:s3:::nobuyuf-tforg-test01-vpcflowlogs"
 }
+
+module "private-vpc-3az" {
+  source = "../../modules/vpc"
+
+  vpcname           = "private-3azVpc"
+  vpc_cidr_block    = "10.99.3.0/24"
+  availability_zone = "3az" // "2az" or "3az"
+  create_igw        = false
+  create_nagtw      = false
+
+  vpcflowlogsbucket = "arn:aws:s3:::nobuyuf-tforg-test01-vpcflowlogs"
+}
+
+module "private-vpc-2az" {
+  source = "../../modules/vpc"
+
+  vpcname           = "private-2azVpc"
+  vpc_cidr_block    = "10.99.2.0/24"
+  availability_zone = "2az" // "2az" or "3az"
+  create_igw        = false
+  create_nagtw      = false
+
+  vpcflowlogsbucket = "arn:aws:s3:::nobuyuf-tforg-test01-vpcflowlogs"
+}
